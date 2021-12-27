724522165ffa04a953eefc99410157586a379004
allexkid <allexsantosrn@gmail.com>
2021-12-27 01:44:56 -0300

    Função liga.
    Criando função que liga a lâmpada.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+/* Mapeando os objetos da página. */
 const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
 
+/* Adicionando listener no clique do botão. */
 turnOn.addEventListener('click', lampOn);
+
+/* Criando função que liga a lâmpada. */
+function lampOn() {
+    lamp.src = './img/ligada.jpg';
+}
+
+
