f93334217466a56a3f0bf190a48d9e6792cba7dc
allexkid <allexsantosrn@gmail.com>
2021-12-27 01:40:46 -0300

    Criando JavaScript
    Criando arquivo JavaScript.

---
+++ script.js
@@ -0,0 +1,5 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+turnOn.addEventListener('click', lampOn);
