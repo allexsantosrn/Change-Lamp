8912bbfcb558343fdbca7a04d1e0c0afc6b02b3c
allexkid <allexsantosrn@gmail.com>
2021-12-27 01:46:14 -0300

    Função desligar
    Criando função que desliga a lâmpada.

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const turnOn = document.getElementById('turnOn');
 const turnOff = document.getElementById('turnOff');
 const lamp = document.getElementById('lamp');
 
-/* Adicionando listener no clique do botão. */
+/* Adicionando listener no clique do botão ligar. */
 turnOn.addEventListener('click', lampOn);
+
+/* Adicionando listener no clique do botão desligar. */
+turnOff.addEventListener('click', lampOff);
 
 /* Criando função que liga a lâmpada. */
 function lampOn() {
     lamp.src = './img/ligada.jpg';
 }
 
+/* Criando função que desliga a lâmpada. */
+function lampOff() {
+    lamp.src = './img/desligada.jpg';
+}
 
